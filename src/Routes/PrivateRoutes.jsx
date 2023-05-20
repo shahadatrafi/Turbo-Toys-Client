1c2cd1c1d98870cc942828d4c816e2f17507d421
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-20 13:10:43 +0600

    added loading state

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -5,8 +5,15 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoutes = ({ children }) => {
 
-    const { user } = useContext(AuthContext);
+    const { user, loading} = useContext(AuthContext);
     const location = useLocation();
+    console.log(user);
+
+    if (loading) {
+        return <div className='min-h-screen flex items-center justify-center mx-auto my-auto'>
+            <progress className="progress progress-primary w-64"></progress>
+        </div>
+    }
 
     if (user) {
         return children;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -9,13 +9,16 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const userSignUp = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
 
     const userLogin = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
@@ -26,6 +29,7 @@ const AuthProvider = ({ children }) => {
     const googleProvider = new GoogleAuthProvider();
 
     const googleSignIn = () => {
+        setLoading(true)
         return signInWithPopup(auth, googleProvider)
     }
 
@@ -33,6 +37,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             console.log(currentUser)
             setUser(currentUser);
+            setLoading(false);
         });
         return () => {
             return unsubscribe();
@@ -42,6 +47,7 @@ const AuthProvider = ({ children }) => {
     
     const authInfo = {
         user,
+        loading,
         userSignUp,
         userLogin,
         googleSignIn,
